9fe2101ecda55fc792e517170fc6a86228d31137
Bryan <bryanferrando59@gmail.com>
2023-11-03 09:47:26 +0100

    Marvin

---
+++ Before.java
@@ -0,0 +1,10 @@
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target(java.lang.annotation.ElementType.METHOD)
+public @interface Before {
+
+
+
+}
