b679e5a8fde5ef8410c15120b1c56b5ca5ae24bf
Bryan <bryanferrando59@gmail.com>
2023-11-03 10:05:02 +0100

    Marvin

--- TestRunner.java
+++ TestRunner.java
@@ -1,20 +1,75 @@
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestRunner {
 
     public void runTests(Class<?> clazz){
         Method[] methods = clazz.getDeclaredMethods();
+
+        List<Method> beforeMethods = new ArrayList<>();
+        List<Method> afterMethods = new ArrayList<>();
+        List<Method> beforeClassMethods = new ArrayList<>();
+        List<Method> afterClassMethods = new ArrayList<>();
+
+        for (Method method : methods){
+            Annotation[] annotations = method.getAnnotations();
+            for (Annotation annotation : annotations){
+                if (annotation instanceof Before){
+                    beforeMethods.add(method);
+                } else if (annotation instanceof After){
+                    afterMethods.add(method);
+                } else if (annotation instanceof BeforeClass){
+                    beforeClassMethods.add(method);
+                } else if (annotation instanceof AfterClass){
+                    afterClassMethods.add(method);
+                }
+            }
+        }
+
+        for(Method method : beforeClassMethods){
+            try {
+                method.invoke(clazz.newInstance());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
         for (Method method : methods){
             if(method.isAnnotationPresent(Test.class)){
+                for (Method beforeMethod : beforeMethods){
+                    try {
+                        beforeMethod.invoke(clazz.newInstance());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
                 Test test = method.getAnnotation(Test.class);
                 System.out.println(test.name());
                 if(test.enabled()){
                     try {
                         method.invoke(clazz.newInstance());
                     } catch (Exception e) {
-
+                        e.printStackTrace();
                     }
                 }
+
+                for(Method afterMethod : afterMethods){
+                    try {
+                        afterMethod.invoke(clazz.newInstance());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        for(Method method : afterClassMethods){
+            try {
+                method.invoke(clazz.newInstance());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
